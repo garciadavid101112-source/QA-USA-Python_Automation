0424611cf8901f1804f90e1cc188c7a34a223c51
Gamer <garcia.david.101112@gmail.com>
2026-06-29 16:31:18 -0400

    Sprint 7 project

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
+URBAN_ROUTES_URL = " https://cnt-04a52814-2dc2-40bf-9672-4bcc387762f1.containerhub.tripleten-services.com/"
+ADDRESS_FROM = 'East_2nd_street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
+
 # File with constants
